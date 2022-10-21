6476bca1491410f7ea39da7b0df5607be52628a1
sebastian.blechmann <sebastian.blechmann@eonerc.rwth-aachen.de>
2022-10-21 13:07:57 +0200

    docker will start automatically after installing

--- scripts/installation_setup.sh
+++ scripts/installation_setup.sh
@@ -20,6 +20,7 @@ echo \
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo service start docker
 # docker compose
 # sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # sudo chmod +x /usr/local/bin/docker-compose
